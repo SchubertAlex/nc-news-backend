73ebd8eaa1c86f18894e39fb88ecf96cc9d34837
SchubertAlex <schubert.w.alex@gmail.com>
2025-01-28 18:44:07 +0000

    complete 5) GET /api/articles

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const express = require("express");
 const app = express();
 const controller = require("./controller/controller");
 
-app.use(express.json());
-
 // REQUESTS:
 app.get("/api", controller.getEndpoints);
 app.get("/api/topics", controller.getTopics);
 app.get("/api/articles/:article_id", controller.getArticleById);
+app.get("/api/articles", controller.getArticles);
 
 // ERROR-HANDLING MIDDLEWARE:
 app.use((req, res) => {
   res.status(404).send({ error: "Endpoint Not Found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.code === "22P02" || err.code === "23502") {
+    res.status(400).send({ error: "Bad Request" });
+  } else {
+    next(err);
+  }
 });
 
 app.use((err, req, res, next) => {
